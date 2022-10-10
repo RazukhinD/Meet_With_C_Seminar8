c9b15ba6e5144c48e0b1ad3c6f1a990f74822f2c
Dmitrii <razuhindm@gmail.com>
2022-10-10 23:53:32 +0300

    Вторая домашняя задача семинара 8

---
+++ task56/Program.cs
@@ -0,0 +1,84 @@
+﻿using static System.Console;
+Clear();
+Write("введите размер матрицы и минимальный и максимальное значение через пробел: ");
+int[] parametrs = GetArrayFromString(ReadLine()!);
+int[,] matrix = GetMatrixArray(parametrs[0], parametrs[1], parametrs[2], parametrs[3]);
+PrinttMatrix(matrix);
+int[] sumofrows= RowsSum(matrix);
+NumberRowWithMinSum(sumofrows);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int[,] GetMatrixArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] resultMatrix = new int[rows, columns];
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            resultMatrix[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return resultMatrix;
+}
+void PrinttMatrix(int[,] inMatrix)
+{
+    for (int i = 0; i < inMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        {
+            Write($"{inMatrix[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+int[] GetArrayFromString(string parameters)
+{
+    string[] parames = parameters.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] parameterNum = new int[parames.Length];
+    for (int i = 0; i < parameterNum.Length; i++)
+    {
+        parameterNum[i] = int.Parse(parames[i]);
+    }
+    return parameterNum;
+}
+
+int[] RowsSum(int [,] matrix)
+{
+    int[] sum = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+       sum[i]= 0;
+       for (int j = 0; j < matrix.GetLength(1); j++)
+       {
+        sum[i]=sum[i]+matrix[i,j];
+       } 
+    }
+    return sum;
+}
+
+void NumberRowWithMinSum (int[] array)
+{
+    int min = array[0];
+    int index = 1;
+    for (int i = 1; i < array.Length; i++)
+    {
+     if (array[i]<min)   
+     {
+        min = array[i];
+        index=i+1;
+     }
+    }
+    WriteLine($"Строка с минимальной суммой: {index}");
+}
